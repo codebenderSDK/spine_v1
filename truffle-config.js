e7a3a97cbc09c29a02c770e2e6116c75aac94c2d
stereoIII6 <stereo@iii6.xyz>
2025-02-27 19:34:58 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -21,10 +21,3 @@ module.exports = {
   contracts_directory: './contracts/',
   contracts_build_directory: './src/contracts/'
 };
-
-// migrations/1_deploy_contracts.js
-const SimpleStorage = artifacts.require("SimpleStorage");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-};
