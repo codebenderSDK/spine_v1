f9b7d2896ad90574c290e628824691c5ed5f41b9
stereoIII6 <stereo@iii6.xyz>
2025-03-04 18:20:36 +0100

    Update Bend.sol

--- contracts/Bend.sol
+++ contracts/Bend.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Bend is ERC20 {
+    constructor(string memory _n, string memory _s) ERC20(_n,_s){
+        _mint(msg.sender,1000);
+    }
+}
